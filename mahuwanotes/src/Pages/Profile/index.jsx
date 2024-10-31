8d2ba60bc8bff252d54fc66e963aa64a99245413
Matheus Chagas <matheuschagas29@gmail.com>
2024-10-30 22:41:01 -0300

    inicio de profile

---
+++ mahuwanotes/src/Pages/Profile/index.jsx
@@ -0,0 +1,44 @@
+import { FiArrowLeft,FiUser, FiMail, FiLock} from 'react-icons/fi';
+import {Input} from '../../componets/Input';
+import {Container} from './styles';
+import {Button} from '../../componets//Button';
+import {Form} from './styles';
+
+
+export function Profile (){
+  return(
+    <Container>
+      <header>
+        <a href="/"> <FiArrowLeft/>
+        </a>
+      </header>
+      <Form>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="Nova atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
