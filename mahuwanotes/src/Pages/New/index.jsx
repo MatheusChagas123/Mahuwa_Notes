c98994aa95f338d9a6a18ed6d8f632e37f6d5c81
Matheus Chagas <matheuschagas29@gmail.com>
2024-11-06 06:17:32 -0300

    header New e espaço de título

--- mahuwanotes/src/Pages/New/index.jsx
+++ mahuwanotes/src/Pages/New/index.jsx
@@ -1,11 +1,23 @@
-import { Header } from '../../components/Header'
+import { Header } from '../../componets/Header'
+import { Input } from '../../componets/Input'
 
-import { Container } from './styles'
+import { Container, Form } from './styles'
 
 export function New() {
   return (
     <Container>
       <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+        </Form>
+      </main>
     </Container>
   )
 }
