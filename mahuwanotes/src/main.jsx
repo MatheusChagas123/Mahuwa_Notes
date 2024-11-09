b913e0e51819708eb887189c94cdb87ad12815ca
Matheus Chagas <matheuschagas29@gmail.com>
2024-11-08 23:32:31 -0300

    Rotas internas no app

--- mahuwanotes/src/main.jsx
+++ mahuwanotes/src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 
-import theme from './styles/theme'
+import theme from './styles/theme';
 
-import { New } from './Pages/New'
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
