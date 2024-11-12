10b49c6d52765049e8a5c97db38fa9793da675f0
Matheus Chagas <matheuschagas29@gmail.com>
2024-11-12 02:16:15 -0300

    após autorizar permitir usuário entrar no home

--- mahuwanotes/src/Pages/Signin/index.jsx
+++ mahuwanotes/src/Pages/Signin/index.jsx
@@ -13,10 +13,10 @@ export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { signIn } = useAuth();
+  const {signIn} = useAuth();
 
   function handleSignIn() {
-    signIn({ email, password });
+    signIn({ email, password });   
   }
 
   return (
--- mahuwanotes/src/hooks/auth.jsx
+++ mahuwanotes/src/hooks/auth.jsx
@@ -7,12 +7,11 @@ function AuthProvider({children}){
   const [data,setData] = useState({});
   async function signIn({ email, password }) {
     try {
-      const response = await api.post("/sessions", { email, password });
-      const {user,token} = response.data;
-      api.defaults.headers.authorization = `Barer${token}`;
-
-      setData({data,setData})
-       
+      const response = await api.post("/sessions",{ email, password });
+      const { user,token } = response.data;
+       api.defaults.headers.authorization = `Bearer ${token}`; 
+       setData({user,token});
+       console.log(user);
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
@@ -22,7 +21,7 @@ function AuthProvider({children}){
     }
   };
   return(
-    <AuthContext.Provider value ={{signIn,user:data.user}}>
+    <AuthContext.Provider value ={{signIn,user: data.user}}>
       {children}
     </AuthContext.Provider>
   )
@@ -34,4 +33,5 @@ function useAuth(){
 }
 
 export { AuthProvider,useAuth}
+
 
